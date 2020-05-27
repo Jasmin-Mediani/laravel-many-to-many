01b0406e62c1f7e8d5ddf077f74157bd57b5721f
Jasmin-Mediani <molihua1992@gmail.com>
2020-05-27 20:39:16 +0200

    ancora prova con jQuery in login.php...

--- public/js/prova.js
+++ public/js/prova.js
@@ -1,1 +1,16 @@
-console.log("ciao");
+
+
+
+// $(document).ready(function () {
+
+//     $(".btn-primary").mouseover(function () {
+//         $(this).toggle();
+//         //console.log("mouseover");
+//     });
+
+//     $(".btn-primary").mouseleave(function () {
+//         $(this).toggle();
+//         //console.log("mouseleave");
+//     });
+
+// });
